8c3c0ea9635f455050027f402e73085c9c8cd40e
Catherine Loesch <catherine.loesch@protonmail.com>
2021-06-24 19:56:49 +0100

    lost bug context solutions

--- index.js
+++ index.js
@@ -11,7 +11,82 @@ let configuration = {
   ]
 }
 
-let printCard = function() {
+
+//  Lost Context Bug:
+let printCardA = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+
+  this.signatories.forEach(function(signatory){
+      let message = `${this.closing[signatory]}, ${signatory}`
+      console.log(message)
+  })
+}
+    // printCardA.call(configuration)
+
+
+
+// Debugging: Discovering the Nature of the Lost Context Bug
+let printCardB = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+
+  console.log("Debug Before forEach: " + this)
+  this.signatories.forEach(function(signatory){
+      console.log("Debug Inside: " + this)
+      // let message = `${this.closing[signatory]}, ${signatory}`
+      // console.log(message)
+  })
+}
+    // printCardB.call(configuration)
+
+    
+
+// Solution 1A: Use a thisArg to avoid the lost context bug
+let printCard1A = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+
+  this.signatories.forEach(function(signatory){
+      let message = `${this.closing[signatory]}, ${signatory}`
+      console.log(message)
+  }, this)
+}
+printCard1A.call(configuration)
+
+
+
+// Solution 1B: invoke bind on the function expression in the forEach:
+let printCard1B = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+  let contextBoundForEachExpr = function(signatory){
+      let message = `${this.closing[signatory]}, ${signatory}`
+      console.log(message)
+  }.bind(this)
+
+  this.signatories.forEach(contextBoundForEachExpr)
+}
+printCard1B.call(configuration)
+
+
+// Solution 2: Use a Closure to Regain Access to the Lost Context
+let printCard2 = function() {
+  console.log(this.frontContent)
+  console.log(this.insideContent)
+
+  let outerContext = this
+
+  this.signatories.forEach(function(signatory){
+      let message = `${outerContext.closing[signatory]}, ${signatory}`
+      console.log(message)
+  })
+}
+printCard2.call(configuration)
+
+
+// Solution 3: Use an Arrow Function Expression to Create a Function Without Its Own Context
+let printCard3 = function() {
   console.log(this.frontContent)
   console.log(this.insideContent)
   // Wow! Elegant! And notice the arrow function's `this` is the same
@@ -20,5 +95,6 @@ let printCard = function() {
   this.signatories.forEach(s => console.log(`${this.closing[s]}, ${s}`)
   )
 }
+printCard3.call(configuration)
 
-printCard.call(configuration)
+
